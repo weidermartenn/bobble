e723bd8ae280ed0d4f800088bc74346e70f3474b
weidermartenn <weidermartenn@gmail.com>
2024-04-10 00:13:05 +0300

    json realization

--- bobble/myform.py
+++ bobble/myform.py
@@ -1,14 +1,51 @@
 from bottle import post, request
-import pdb
+import json
 
 @post('/home')
 def my_form():
-    questions = {}
     quest = request.forms.get('QUEST')
     name = request.forms.get('NAME')  
     mail = request.forms.get('ADRESS') 
     date_str = request.forms.get('DATE')   
     
-    questions.setdefault(mail, [name, quest])  # Changed to set list [name, quest] as value for 'mail' key
-    pdb.set_trace()
-    return("Thanks, %s! The answer will be sent to the email %s Access Date: %s" % (name, mail, date_str))
+    # Проверка на корректность данных в поле вопроса
+    if len(quest) <= 3 or quest.isdigit():
+        return "Please enter a valid question (more than 3 characters and not just digits)."
+    
+    # Проверка на корректность данных в поле почты пользователя
+    if '@' not in mail or '.' not in mail:
+        return "Please enter a valid email address."
+    
+    # Создание новой записи в формате словаря, включая все поля
+    new_entry = {
+        'NAME': name,
+        'ADRESS': mail,
+        'DATE': date_str,
+        'QUESTIONS': []
+    }
+    
+    try:
+        # Открытие файла JSON для чтения
+        with open('data.json', 'r') as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                # Если файл пустой или не содержит валидного JSON, создаем пустой словарь
+                data = {}
+    except FileNotFoundError:
+        # Если файл не существует, создаем пустой словарь
+        data = {}
+    
+    # Добавление новой записи или обновление существующей
+    if mail in data:
+        if quest not in data[mail]['QUESTIONS']:  # Проверка на дубликаты
+            data[mail]['QUESTIONS'].append(quest)
+    else:
+        data[mail] = new_entry
+        data[mail]['QUESTIONS'].append(quest)
+    
+    # Запись данных в файл JSON
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    
+    return "Thanks, %s! The answer will be sent to the email %s. Access Date: %s" % (name, mail, date_str)
