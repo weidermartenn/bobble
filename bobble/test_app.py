ad24e5e6ac01d0c7a0f05c50bb5723c7f8c6732d
Oleg Krivyonishev <leshiy.vl5@gmail.com>
2024-05-28 13:54:08 +0300

    Снова обновил содержимое test_app.py

--- bobble/test_app.py
+++ bobble/test_app.py
@@ -5,15 +5,25 @@ from utils import check_email
 from myform import my_form
 
 class TestApp(unittest.TestCase):
-    # Тест правильных почт
-    def test_check_email_valid(self):
+    def test_check_email_valid_default(self):
         self.assertTrue(check_email('example@gmail.com'))
-        self.assertTrue(check_email('user_name@mail.ru'))
+        self.assertTrue(check_email('username@mail.ru'))
+    
+    def test_check_email_valid_underline(self):
+        self.assertTrue(check_email('exa_mple@gmail.com'))
+        self.assertTrue(check_email('U_ser_name@gmail.com'))
+    
+    def test_check_email_valid_with_digits(self):
+        self.assertTrue(check_email('example123@gmail.com'))
+        self.assertTrue(check_email('User_123name@gmail.com'))
 
-    # Тест неправильных почт
-    def test_check_email_invalid(self):
+    def test_check_email_invalid_default(self):
         self.assertFalse(check_email('invalid_email'))
         self.assertFalse(check_email('example@example'))
+    
+    def test_check_email_valid_symbols(self):
+        self.assertFalse(check_email('?????@gmail.com'))
+        self.assertFalse(check_email('!%%@gmail.com'))
 
 if __name__ == '__main__':
     unittest.main()
